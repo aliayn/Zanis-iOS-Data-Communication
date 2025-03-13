9b6e808374b980e049ed3b0c09c895418e47fe6c
AliAynechian <aliaynechiyan@taraz.org>
2025-03-13 13:17:36 +0330

    Update .gitignore to exclude generated files and add Injectable annotation to HomeCubit for dependency injection

--- lib/home/cubit/home_cubit.dart
+++ lib/home/cubit/home_cubit.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'home_state.dart';
 part 'home_cubit.freezed.dart';
 
+@injectable
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeState.initial());
 }
